82f3af939480fb9f96b2072c3290452889f8c510
Stephanie Ananth <stephkananth@gmail.com>
2020-03-26 15:24:57 -0400

    initial commit

---
+++ Streaks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Streaks
+//
+//  Created by Steph Ananth on 3/26/20.
+//  Copyright © 2020 Steph K. Ananth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
